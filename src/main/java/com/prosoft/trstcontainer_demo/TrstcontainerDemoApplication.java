bca4b7126e3ec1691bc8874fe65405cacd0337a8
sproshchaev <sproshchaev@gmail.com>
2026-04-06 14:27:02 +0500

    init

---
+++ src/main/java/com/prosoft/trstcontainer_demo/TrstcontainerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prosoft.trstcontainer_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrstcontainerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrstcontainerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prosoft/trstcontainer_demo/TrstcontainerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosoft.trstcontainer_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrstcontainerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
